6addbd40349fd27b295c6f21a693ba262c19ff59
mcps5601 <mcps5601@gmail.com>
2023-06-14 00:49:22 +0800

    Add fine-tuning script

---
+++ finetune.sh
@@ -0,0 +1,25 @@
+deepspeed --num_gpus 2 --num_nodes 1 \
+fastchat/train/train.py \
+    --model_name_or_path ~/llama-7b-hf  \
+    --data_path data/dummy_conversation.json \
+    --bf16 False \
+    --fp16 True \
+    --output_dir output_vicuna \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1200 \
+    --save_total_limit 10 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 False \
+    --model_max_length 768 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True \
+    --deepspeed ./deepspeed.json
